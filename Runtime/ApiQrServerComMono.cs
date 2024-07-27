2969ee6feb0b90315b9786479148da09334964af
Éloi Strée <eloistree@gmail.com>
2024-07-28 01:37:11 +0200

    Save: 07/28/2024 1:37 AM

--- Runtime/ApiQrServerComMono.cs
+++ Runtime/ApiQrServerComMono.cs
@@ -1,50 +1,11 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Networking;
 
-public class ApiQrServerComMono : MonoBehaviour
-{
 
-    public int m_size = 256;
-    public string m_textProvideToUrl="Hello World";
-    public string m_serverUri = "https://api.qrserver.com/v1/create-qr-code/?size={1}x{1}&data={0}";
-
-    public string m_lastCalledUrl;
-    public Texture2D m_lastDownloadedQR;
-
-    public UnityEvent<Texture2D> m_onDownloadedQR;
-
-    [ContextMenu("Generate QR")]
-    public void GenerateFromInspectorQR()
+public class ApiQrServerComMono  : LoadQrFromUrlMono
+{ 
+    private void Reset()
     {
-        GenerateQR(m_textProvideToUrl, m_size);
-    }
-    public void GenerateQR(string text, int size = 256)
-    {
-        m_lastCalledUrl = string.Format(m_serverUri, text, size);
-        StartCoroutine(DownloadQR(m_lastCalledUrl));
-
-
-    }
-
-    private IEnumerator DownloadQR(string url)
-    {
-        using (UnityWebRequest request = UnityWebRequestTexture.GetTexture(url))
-        {
-            yield return request.SendWebRequest();
-
-            if (request.result != UnityWebRequest.Result.Success)
-            {
-                m_lastDownloadedQR = null;
-            }
-            else
-            {
-                m_lastDownloadedQR = ((DownloadHandlerTexture)request.downloadHandler).texture;
-            }
-            m_onDownloadedQR.Invoke(m_lastDownloadedQR);
-        }
+        m_serverUri = "https://api.qrserver.com/v1/create-qr-code/?size={1}x{1}&data={0}";
     }
 }
---
+++ Runtime/QuickChartIoMono.cs
@@ -0,0 +1,7 @@
+﻿public class QuickChartIoMono : LoadQrFromUrlMono
+{
+    private void Reset()
+    {
+        m_serverUri = "https://quickchart.io/qr?size={1}x{1}&text={0}";
+    }
+}
